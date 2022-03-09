47883cd3a4742ba590285f00b45d290a3caef523
pant411 <pantchanit2561@hotmail.com>
2022-03-09 22:29:54 +0700

    update

--- backend/app.py
+++ backend/app.py
@@ -10,30 +10,35 @@ app = Flask(__name__)
 #pots = mydb["Pots"]
 
 def connect():
-    myclient = pymongo.MongoClient("mongodb+srv://chuncheiw_team:o6bROnEYRtsiRle2@cluster0.jjsfz.mongodb.net/Geometry_Pot") #My test server link that will change when you install in another server 
+    # My test server link that will change when you install in another server
+    myclient = pymongo.MongoClient(
+        "mongodb+srv://chuncheiw_team:o6bROnEYRtsiRle2@cluster0.jjsfz.mongodb.net/test?retryWrites=true&w=majority", tls=True, tlsAllowInvalidCertificates=True)
     mydb = myclient["Geometry_Pot"]
     print(mydb)
     return mydb
+
 
 def addpots(data):
     mydb = connect()
     pots = mydb["Pots"]
     newd = {
-            "Name" : data["Name"],
-            "Lighttime" : [8.00, 18.00],
-            "HumidityLV" : 0,
-            "TemperatureLV" : 0,
+        "Name": data["Name"],
+        "Lighttime": [8.00, 18.00],
+        "HumidityLV": 0,
+        "TemperatureLV": 0,
     }
     pots.insert_one(newd)
     return newd
 
+
 db = connect()
 print(db)
 
+
 @app.route('/', methods=["GET"])
 def home():
-    #print(.collection)
-    
+    # print(.collection)
+
     return "Hello"
 
 
@@ -41,8 +46,10 @@ def home():
 def insert():
     body = request.get_json()
     print(body)
-    #x = mycol.insert_one(body)
+    mydb = connect()
+    x = mydb["test1"].insert_one(body)
     return "success"
+
 
 @app.route("/add_pot", methods=["POST"])
 def add_pot():
@@ -53,8 +60,10 @@ def add_pot():
     d = addpots(data)
     print(d)
     return "ok"
-    #except:
+
+
+    # except:
     #    return "fail"
 if __name__ == "__main__":
-    #print("hello")
+    # print("hello")
     app.run(debug=True, port=5555)
