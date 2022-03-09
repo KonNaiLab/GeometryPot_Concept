51290584388beed3435544a497d9ecd13601b83e
tamitamboommm!! <pun.hoo.hoo@gmail.com>
2022-03-09 21:46:40 +0700

    Got problem with mongdb link

---
+++ backend/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify, make_response
+import pymongo
+import os
+
+app = Flask(__name__)
+
+
+#mydb = myclient["Geometry_Pot"]
+#mycol = mydb["test1"]
+#pots = mydb["Pots"]
+
+def connect():
+    myclient = pymongo.MongoClient("mongodb+srv://chuncheiw_team:o6bROnEYRtsiRle2@cluster0.jjsfz.mongodb.net/Geometry_Pot") #My test server link that will change when you install in another server 
+    mydb = myclient["Geometry_Pot"]
+    print(mydb)
+    return mydb
+
+def addpots(data):
+    mydb = connect()
+    pots = mydb["Pots"]
+    newd = {
+            "Name" : data["Name"],
+            "Lighttime" : [8.00, 18.00],
+            "HumidityLV" : 0,
+            "TemperatureLV" : 0,
+    }
+    pots.insert_one(newd)
+    return newd
+
+db = connect()
+print(db)
+
+@app.route('/', methods=["GET"])
+def home():
+    #print(.collection)
+    
+    return "Hello"
+
+
+@app.route('/add_data', methods=["POST"])
+def insert():
+    body = request.get_json()
+    print(body)
+    #x = mycol.insert_one(body)
+    return "success"
+
+@app.route("/add_pot", methods=["POST"])
+def add_pot():
+    data = request.get_json()
+    print(data["Name"])
+    print(data)
+    print("add to mongo")
+    d = addpots(data)
+    print(d)
+    return "ok"
+    #except:
+    #    return "fail"
+if __name__ == "__main__":
+    #print("hello")
+    app.run(debug=True, port=5555)
