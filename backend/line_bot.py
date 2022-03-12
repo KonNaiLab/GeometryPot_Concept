8d93e9339bb15e9660330cda3c35e6c71afdf038
Phutthawan <phutthawan.h@ku.th>
2022-03-12 13:21:55 +0700

    ส่วน code ของ linebot

--- backend/line_bot.py
+++ backend/line_bot.py
@@ -1,4 +1,5 @@
 from flask import Flask, abort, request, jsonify, make_response
+from numpy import array
 import pymongo
 import os
 
@@ -19,6 +20,15 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi('ikDp2ap89gvpkrB4f9djOEfXSvvAyDcZ+yhKTfU+90oaTOsJn/AZdk51VK1rnnl7Hv19eJukOsx0YQWga1d76FlNNH0B+7Li23iOKUkL1nMkNpJgRuyKw1k/CLsY6ivV1wyTuzGV84B54xsq5tE30QdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('69b00f0f5fae1c6f51cda305a4acf289')
+
+'''
+zone={"เขตอบอุ่น":{"light" : [9,16],"humid" :56,"temp" :30},
+"เขตร้อน":{"light" : [9,16],"humid" :56, "temp" :35},
+"เขตแห้งแล้ง":{"light" : [9,16],"humid" :45,"temp" :35}}
+'''
+zone={"เขตอบอุ่น":{"humid" :56,"temp" :30},
+"เขตร้อน":{"humid" :56, "temp" :35},
+"เขตแห้งแล้ง":{"humid" :30,"temp" :34}}
 
 def connect():
     # My test server link that will change when you install in another server
@@ -61,7 +71,7 @@ def handle_message(event):# สถานะ ตั้งค่า โซล help
     if message.type == "text":
         text_message = message.text
         space_separated_message =text_message.strip().split(" ")
-        if "!สถานะ"==text_message and len(space_separated_message)!=2:
+        if "!สถานะ"==space_separated_message[0] and len(space_separated_message)==2: #!สถานะ 1
             if type(space_separated_message[1])!=type(int):
                 defalut_reply(token)
                 return
@@ -79,13 +89,29 @@ def handle_message(event):# สถานะ ตั้งค่า โซล help
             message_reply="สถานะ\n ระดับน้ำในระดับที่กำหนด : {tank}\n ระดับความชื้น : {humid}\n หลอดไฟ : {light}\n อุณหภูมิ : {temp}"
             reply_message(token, message_reply)
             return 
-        elif "!ตั้งค่า" == text_message and len(space_separated_message)!=3:
-            
+        elif "!ตั้งค่า" == space_separated_message[0]: #and (len(space_separated_message)!=4 or len(space_separated_message)!=5): # !ตั้งค่า 1 ไฟ ค่าที่ต้องการ
+            # !ตั้งค่า 1 ไฟ 0 5
+            modedict={"หลอดไฟ":"light","ระดับความชื้น":"humid","อุณหภูมิ":"temp"}
+            if space_separated_message[2]!="หลอดไฟ" and len(space_separated_message)==5:
+                new_data=[int(space_separated_message[3]),int(space_separated_message[4])]
+            else:
+                new_data=space_separated_message[3]
+            data=setpot({"data":new_data},space_separated_message[1],modedict[space_separated_message[2]])
             return 
-        elif "!โซน"==text_message:
-            return 
-        elif "!help"==text_message:
-            return 
+        elif "!โซน"==space_separated_message[0]:# !โซน ร้อน 1 || โซน ข้อมูล
+            if space_separated_message[1]=="ข้อมูล" and len(space_separated_message)==2:
+                message_reply2="เขตอบอุ่น: ความชื้น 56% อุณหภูมิ 30 c\nเขตร้อน ความชื้น 56% อุณหภูมิ 35 c\nเขตแห้งแล้ง ความชื้น 30% อุณหภูมิ 34 c}"
+                reply_message(token, message_reply2)
+            elif len(space_separated_message)==3 and (space_separated_message[1] in zone):
+                data=setpot({"data":zone[space_separated_message[1]]["humid"]},space_separated_message[2],"humid")
+                data=setpot({"data":zone[space_separated_message[1]]["temp"]},space_separated_message[2],"temp")
+            return
+        elif "!help"==space_separated_message[0]:
+            if len(space_separated_message)==1:
+                message_reply="!สถานะ <ลำดับกระถาง> = แสดงสถานะของกระถานนั้น\n!ตั้งค่า <ลำดับกระถาง> <โหมดที่ต้องการ> <ค่าที่ต้องการ> = เปลี่ยนค่าที่ต้องการจะเปลี่ยน\n!ตั้งค่า <ลำดับกระถาง> หลอดไฟ <เวลาเริ่มต้น> <เวลาสิ้นสุด> = เปลี่ยนระยะเวลาฉายไฟ\n!โซน = แสดงข้อมูลโซนที่ให้เลือกทั้งหมด\n!โซน <เขตที่ต้องการ> <ลำดับกระถาง> = เปลี่ยนค่าตามโซลที่เลือก"
+                reply_message(token, message_reply)
+                return
+            return
         else:
             defalut_reply(token)
         return
